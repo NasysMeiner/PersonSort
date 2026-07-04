4b6efbcda3328e4154f3e5f98353ffb8510ed88f
gerelDan <gerel@bk.ru>
2026-07-05 00:54:27 +0200

    person
    commit 2 times

---
+++ src/main/java/Person.java
@@ -0,0 +1,56 @@
+public class Person implements Comparable<Person>{
+
+    private final String name;
+    private final String mail;
+    private final String password;
+
+    private Person(Builder builder) {
+        this.name = builder.name;
+        this.mail = builder.mail;
+        this.password = builder.password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    String getPassword() {
+        return password;
+    }
+    @Override
+    public int compareTo(Person o) {
+        return 0;
+    }
+
+    public static class Builder {
+        private String name;
+        private String mail;
+        private String password;
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder mail(String mail) {
+            this.mail = mail;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Person build() {
+            if (name == null || mail == null || password == null) {
+                throw new IllegalStateException("Все поля должны быть заполнены");
+            }
+            return new Person(this);
+        }
+    }
+}
