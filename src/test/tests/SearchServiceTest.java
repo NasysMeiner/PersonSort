06067ecada79e2944af979978293a5d471d0059c
NasusMain <107574485+NasysMeiner@users.noreply.github.com>
2026-07-14 22:56:40 +0300

    Correct test

--- src/test/tests/SearchServiceTest.java
+++ src/test/tests/SearchServiceTest.java
@@ -5,12 +5,29 @@ import service.SearchService;
 import test.Test;
 
 public class SearchServiceTest extends Test {
-
+    private SearchService searchService;
+    private Person[] testData;
+    
     @Override
     public void run() {
-        SearchService searchService = new SearchService();
-
-        Person[] data = {new Person.Builder().setName("Alex").setPassword("123456").setMail("mail@mail.com").build(), 
+        searchService = new SearchService();
+        testData = createTestData();
+        
+        testSearchByMail();
+        testSearchByPassword();
+        testSearchByName();
+        testSearchByNonExistentName();
+        testSearchWithEmptyCollection();
+        
+        if (getCode() != 0) {
+            setSuccess();
+            setMessage("All SearchService tests passed!");
+        }
+    }
+    
+    private Person[] createTestData() {
+        return new Person[] {
+            new Person.Builder().setName("Alex").setPassword("123456").setMail("mail@mail.com").build(),
             new Person.Builder().setName("Anna").setPassword("123456").setMail("mail@mail.com").build(),
             new Person.Builder().setName("Oleg").setPassword("sdfsdf23").setMail("mail@mail.com").build(),
             new Person.Builder().setName("Mariya").setPassword("asasdc").setMail("mail@mail.com").build(),
@@ -18,60 +35,81 @@ public class SearchServiceTest extends Test {
             new Person.Builder().setName("Aleksandr").setPassword("sqweddf").setMail("mail@mail.com").build(),
             new Person.Builder().setName("Vlad").setPassword("cxcew3").setMail("mail@mail.com").build()
         };
-
-        String targetMail = "mail@mail.com";
-        long countMail = searchService.searchElements(person -> person.getMail().equals(targetMail), data);
-        long expectedMail = 7;
-
-        if(countMail != expectedMail) {
-            setFail();
-            setMessage("Not correct SearchService in mail!");
-            return;
-        }
-
-        String targetPassword = "123456";
-        long countPassword = searchService.searchElements(person -> person.getPassword().equals(targetPassword), data);
-        long expectedPassword = 3;
-
-        if(countPassword != expectedPassword) {
-            setFail();
-            setMessage("Not correct SearchService in password!");
-            return;
-        }
-
-        String targetName = "Anna";
-        long countName = searchService.searchElements(person -> person.getName().equals(targetName), data);
-        long expectedName = 1;
-
-        if(countName != expectedName) {
-            setFail();
-            setMessage("Not correct SearchService in name!");
-            return;
-        }
-        
-        String targetEmptyName = "Empty";
-        long countEmptyName = searchService.searchElements(person -> person.getName().equals(targetEmptyName), data);
-        long expectedEmptyName = 0;
-
-        if(countEmptyName != expectedEmptyName) {
-            setFail();
-            setMessage("Not correct SearchService in empty name!");
-            return;
-        }
-
-        data = new Person[0];
-        String target = "Oleg";
-        long count = searchService.searchElements(person -> person.getName().equals(target), data);
-        long expected = 0;
-
-        if(count != expected) {
-            setFail();
-            setMessage("Not correct SearchService in empty collection!");
-            return;
-        }
-
-        setSuccess();
-        setMessage("Correct work");
     }
     
+    private void testSearchByMail() {
+        String target = "mail@mail.com";
+        long expected = 7;
+        long actual = searchService.searchElements(
+            person -> person.getMail().equals(target), 
+            testData
+        );
+        
+        if (actual != expected) {
+            setFail();
+            setMessage("Search by mail failed! Expected: " + expected + 
+                      ", but was: " + actual + " for target: " + target);
+        }
+    }
+    
+    private void testSearchByPassword() {
+        String target = "123456";
+        long expected = 3;
+        long actual = searchService.searchElements(
+            person -> person.getPassword().equals(target), 
+            testData
+        );
+        
+        if (actual != expected) {
+            setFail();
+            setMessage("Search by password failed! Expected: " + expected + 
+                      ", but was: " + actual + " for target: " + target);
+        }
+    }
+    
+    private void testSearchByName() {
+        String target = "Anna";
+        long expected = 1;
+        long actual = searchService.searchElements(
+            person -> person.getName().equals(target), 
+            testData
+        );
+        
+        if (actual != expected) {
+            setFail();
+            setMessage("Search by name failed! Expected: " + expected + 
+                      ", but was: " + actual + " for target: " + target);
+        }
+    }
+    
+    private void testSearchByNonExistentName() {
+        String target = "Empty";
+        long expected = 0;
+        long actual = searchService.searchElements(
+            person -> person.getName().equals(target), 
+            testData
+        );
+        
+        if (actual != expected) {
+            setFail();
+            setMessage("Search by non-existent name failed! Expected: " + expected + 
+                      ", but was: " + actual + " for target: " + target);
+        }
+    }
+    
+    private void testSearchWithEmptyCollection() {
+        Person[] emptyData = new Person[0];
+        String target = "Oleg";
+        long expected = 0;
+        long actual = searchService.searchElements(
+            person -> person.getName().equals(target), 
+            emptyData
+        );
+        
+        if (actual != expected) {
+            setFail();
+            setMessage("Search in empty collection failed! Expected: " + expected + 
+                      ", but was: " + actual + " for target: " + target);
+        }
+    }
 }
